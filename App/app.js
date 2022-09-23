631704e825a0ce3fb06ec690203fd8a2c2176a68
Patrick <patrick.czaczka98@gmail.com>
2022-09-23 15:50:23 +1000

    app.js kinda working

--- App/app.js
+++ App/app.js
@@ -1,11 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectId;
+// var ObjectID = require('mongodb').ObjectId;
 
 const url = 'mongodb://localhost:27017';
+const dbName = 'mydb';
+// const client = new MongoClient(url);
+// const colName = 'products'
 
-MongoClient.connect(url,function(err, client) {
-    if (err) {return console.log(err)}
-
-    const dbName = 'mydb';
+MongoClient.connect(url, {useNewUrlParser:true}, function(err,client) {
+    console.log("connect to server");
     const db = client.db(dbName);
+    // const collection = db.collection(colName);
+    client.close();
 });
